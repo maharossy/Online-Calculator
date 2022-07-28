b74603b8a88b474c3d95533823b6d53f99de1af7
Vladyslav Marynets <smarynec@gmail.com>
2022-07-28 18:10:33 +0300

    total credit

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -1,7 +1,12 @@
+import {priceFormatter} from './formatters.js';
+
 // Inputs
 const inputCost = document.querySelector('#input-cost'),
       inputDownPayment = document.querySelector('#input-downpayment'),
-      inputTerm = document.querySelector('#input-term');
+      inputTerm = document.querySelector('#input-term'),
+      form = document.querySelector('#form'),
+      totalCost = document.querySelector('#total-cost'),
+      totalMonthPayment = document.querySelector('#total-month-payment');
 
 // Cleave options formating
 const cleavePriseSetting = {
@@ -13,5 +18,34 @@ const cleavePriseSetting = {
 
 // Launch formating cleave
 const cleaveCost = new Cleave(inputCost, cleavePriseSetting),
-      cleaveDownPayment = new Cleave(inputDownPayment, cleavePriseSetting);
+    cleaveDownPayment = new Cleave(inputDownPayment, cleavePriseSetting),
 
+    cleaveTerm = new Cleave(inputTerm, cleavePriseSetting);
+
+calcMortgage();
+
+
+// Show and calculation of the loan amount
+form.addEventListener('input', () => {
+    // Credit amount
+    calcMortgage();
+});
+
+
+function calcMortgage() {
+    //Total credit amount
+    const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
+    totalCost.innerText = priceFormatter.format(totalAmount);
+
+    //mortgage term
+    const creditRate = +document.querySelector('input[name="program"]:checked').value;
+    const mounthRate = creditRate / 12;
+
+    //Credit years
+    const years = +cleaveTerm.getRawValue();
+    const months = years * 12;
+
+    //Monthly payment calculation
+    const mounthPayment = (totalAmount * mounthRate) / 1 - (1 + mounthRate) * (1 - months);
+    totalMonthPayment.innerText = mounthPayment.toFixed(2);
+}
--- js/formatters.js
+++ js/formatters.js
@@ -1,1 +1,5 @@
-export const percentFormatter = new Intl.NumberFormat('ru-RU', {style: 'percent', maximumFractionDigits: 3}); // 4,752 %
+export const percentFormatter = new Intl.NumberFormat('ru-RU',
+    {style: 'percent', maximumFractionDigits: 3}); // 4,752 %
+
+export const priceFormatter = new Intl.NumberFormat('ru-RU',
+    {style: 'currency', currency: 'RUB', maximymFractionDigits: 2}); // 4 000 P
