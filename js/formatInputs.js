a00b8751451549bca12ed0f25f75e665d2637cdf
Vladyslav Marynets <smarynec@gmail.com>
2022-07-29 22:27:31 +0300

    inputs

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -52,6 +52,7 @@ function calcMortgage() {
     totalMonthPayment.innerText = priceFormatterDecimals.format(mounthPayment);
 }
 
+// Slider Cost
 const sliderCost = document.querySelector('#slider-cost');
 
 noUiSlider.create(sliderCost, {
@@ -76,6 +77,63 @@ sliderCost.noUiSlider.on('update', function() {
     const sliderValue = parseInt(sliderCost.noUiSlider.get(true));
 
     cleaveCost.setRawValue(sliderValue);
-    
+
+    calcMortgage();
+});
+
+// Slider Downpayment
+const sliderDownpayment = document.querySelector('#slider-downpayment');
+
+noUiSlider.create(sliderDownpayment, {
+    start: 12000000,
+    connect: 'lower',
+    tooltips: true,
+    step: 100000,
+    range: {
+        min: 0,
+        '50%': [10000000, 1000000],
+        max: 100000000,
+    },
+
+    format: wNumb({
+        decimals: 0,
+        thousand: ' ',
+        suffix: '',
+    }),
+});
+
+sliderDownpayment.noUiSlider.on('update', function() {
+    const sliderValue = parseInt(sliderDownpayment.noUiSlider.get(true));
+
+    cleaveDownPayment.setRawValue(sliderValue);
+
+    calcMortgage();
+});
+
+// Slider years
+const sliderTerm  = document.querySelector('#slider-term');
+
+noUiSlider.create(sliderTerm, {
+    start: 1,
+    connect: 'lower',
+    tooltips: true,
+    step: 1,
+    range: {
+        min: 1,
+        max: 30,
+    },
+
+    format: wNumb({
+        decimals: 0,
+        thousand: '',
+        suffix: '',
+    }),
+});
+
+sliderTerm.noUiSlider.on('update', function() {
+    const sliderValue = parseInt(sliderTerm.noUiSlider.get(true));
+
+    cleaveTerm.setRawValue(sliderValue);
+
     calcMortgage();
 });
